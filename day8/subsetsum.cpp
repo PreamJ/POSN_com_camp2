c1e889fd59b7345664c9c94bb4d2b1ceca1edc67
Pitchapa Junnatasna <114279841+PreamJ@users.noreply.github.com>
2023-03-29 07:40:58 +0700

    Update subsetsum.cpp

--- day8/subsetsum.cpp
+++ day8/subsetsum.cpp
@@ -1,10 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void subsetSum(){
+void printSubset(const vector<int>& subset) {
+    cout << "{ ";
+    for (int i : subset) {
+        cout << i << " ";
+    }
+    cout << "}" << endl;
+}
+bool subsetSum(const vector<int>& set, int target, vector<int>& subset, int index) {
+    if (target == 0) {
+        printSubset(subset);
+        return true;
+    }
 
+    if (index >= set.size() || target < 0) {
+        return false;
+    }
+    subset.push_back(set[index]);
+    bool found = subsetSum(set, target - set[index], subset, index + 1);
+    subset.pop_back();
+    found = subsetSum(set, target, subset, index + 1) || found;
+
+    return found;
 }
 
-int main(){
-    vector <int> setA;
+int main() {
+    int n, target;
+    cout << "Enter the size of the set: ";
+    cin >> n;
+    vector<int> set(n);
+    cout << "Enter the elements of the set: ";
+    for (int i = 0; i < n; i++) {
+        cin >> set[i];
+    }
+    cout << "Enter the target sum: ";
+    cin >> target;
+
+    vector<int> subset;
+    if (!subsetSum(set, target, subset, 0)) {
+        cout << "Thre is no subset that make up to target number." << endl;
+    }
+
+    return 0;
 }
